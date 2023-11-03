e73327ab448c3f9eef232972ce11805cceef57fd
leandromelolm <leandrosiqueira.pe@gmail.com>
2023-11-03 11:25:28 -0300

    chore: updated netlify

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
-
-const test = process.env.TEST_ENV;
-
+const test = process.env.TEST_ENV ?? "arquivo main.js";
 console.log(`ENV: ${test}`);
 
+
+// const TEST_ENV_2 = 'TESTE VARIAVEL DE AMBIENTE';
+// export default TEST_ENV_2;
