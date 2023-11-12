6cf43a12eac6b1eae1041eb42d9891edd9ca1978
leandromelolm <leandrosiqueira.pe@gmail.com>
2023-11-12 13:22:23 -0300

    refactor(app-script-google): function get and post changed

--- src/app-script-google.gs.js
+++ src/app-script-google.gs.js
@@ -1,22 +1,25 @@
-const sheetName = 'Sheet1';
-const scriptProp = PropertiesService.getScriptProperties();
 const urlSpreadSheet = 'URL_PLANILHA';
 const spreadSheetID = 'ID_PLANILHA';
+const sheetName = 'Sheet1';
 const sheets = SpreadsheetApp.openByUrl(urlSpreadSheet);
 const sheet = sheets.getSheetByName(sheetName);
-const spreadsheetAppId = SpreadsheetApp.openById(spreadSheetID);
+const spreadsheetId = SpreadsheetApp.openById(spreadSheetID);
+const sheetUser = sheets.getSheetByName('UserSheet')
 
-// apps-script-google-planilha-requisicao-material v8
+// Versão 9 - Prod
+
+function requestTest() {
+  let itensString = sheetUser.getRange("F"+2).getValue();
+   Logger.log(JSON.stringify(itensString));
+}
 
 // POST
 const doPost = (e) => {
   const lock = LockService.getScriptLock();
   lock.tryLock(10000);
   try {
-    const doc = spreadsheetAppId;
-    // const sheet = doc.getSheetByName(sheetName);
-    const sheet = doc.getSheetByName(e.parameter['sheetName']);   
-
+    const doc = spreadsheetId;
+    const sheet = doc.getSheetByName(e.parameter['sheetName']);
     if (e.parameter['requisicao'] == 'salvar') {
       const headers = sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues()[0];
       const newRow = headers.map((header) => {
@@ -35,35 +38,16 @@ const doPost = (e) => {
           { 'result': 'success', 'row': nextRow }))
         .setMimeType(ContentService.MimeType.JSON);
     } else {
-      const dados = JSON.parse(e.postData.contents);
-      if (dados.requisicao == 'obter') {
-        return ContentService.createTextOutput(
-          JSON.stringify(
-            SpreadsheetApp.openByUrl(dados.link)
-              .getSheetByName(dados.pagina)
-              .getRange(dados.celulas)
-              .getValues()
-          )
-        );
-      } 
-      if (dados.requisicao == 'ultimopedido') {
-        return ContentService.createTextOutput(
-          JSON.stringify(
-            sheet.getLastRow()
-          )
-        );
-      } else {
-        return ContentService.createTextOutput(
-          JSON.stringify({
-            text: "tipo de requisicao invalida",
-            "parameter": e.parameter[header]
-          })).setMimeType(ContentService.MimeType.JSON);
-      }
+      return ContentService.createTextOutput(
+        JSON.stringify({
+          text: "tipo de requisicao invalida",
+          "parameter": e.parameter[header]
+        })).setMimeType(ContentService.MimeType.JSON);
     }
-
   } catch (error) {
     return ContentService
-      .createTextOutput(JSON.stringify({ 'result': 'error', error }))
+      .createTextOutput(JSON.stringify(
+        { 'result': 'error', error }))
       .setMimeType(ContentService.MimeType.JSON);
 
   } finally {
@@ -71,20 +55,32 @@ const doPost = (e) => {
   }
 }
 
-// GET PARAMETROS
-// ?id=&search=?page=&perPage=startId=&endId=
-// ?id=&search=
-// ?page=&perPage=
-// ?search=pesquisarIntervalo&startId=&endId=
-
 // GET
+// ?id=&search=&page=&perPage=&startId=&endId=&search_key=
+// Pesquisar usando startId e endId
+// ?id=&search=pesquisarIntervalo&page=&perPage=&startId=&endId=&search_key=
+
 function doGet(e) {
+  const lock = LockService.getScriptLock();
+  lock.tryLock(10000);
+  try {
   const pesquisaId = e.parameter['id'] || "";
   const pesquisaTxt = e.parameter['search'] || "";
   const page = parseInt(e.parameter['page']) || 1;
   const perPage = parseInt(e.parameter['perPage']) || 20;
   const inicial = parseInt(e.parameter['startId'] || 1);
   const final = parseInt(e.parameter['endId'] || 1);  
+  const searchKey = e.parameter['search_key'] || "";
+
+  if(searchKey === sheetUser.getRange("F"+2).getValue()){
+    let encontrado = encontrarTextoNaFolha(pesquisaTxt)
+    return ContentService.createTextOutput(
+      JSON.stringify({
+        search: pesquisaTxt,
+        size: encontrado.length,
+        content: encontrado
+      })).setMimeType(ContentService.MimeType.JSON);
+  }
 
   if(pesquisaId !== "") {
     let pedido = buscarPorId(pesquisaId);
@@ -107,6 +103,12 @@ function doGet(e) {
 
   if (pesquisaTxt != "" ) {
     let allResults = encontrarTextoNaColuna(pesquisaTxt);
+    if (allResults.resultSize == 0){
+      return ContentService.createTextOutput(
+        JSON.stringify(
+          allResults
+        )).setMimeType(ContentService.MimeType.JSON);
+    }
     allResults.sort((a, b) => b.id - a.id);
     let totalResults = allResults.length;
 
@@ -117,10 +119,33 @@ function doGet(e) {
     });
     return ContentService.createTextOutput(resultString)
       .setMimeType(ContentService.MimeType.JSON);
-  }  
+  }
+
+  } catch (error) {
+  return ContentService
+      .createTextOutput(JSON.stringify(
+        { 'result': 'error', error }))
+      .setMimeType(ContentService.MimeType.JSON);
+
+  } finally {
+    lock.releaseLock();
+  } 
+
+}
+
+function isDigit(n) {
+    return /^\d+$/.test(n)
 }
 
 function buscarPorId(id) {
+  if(isDigit(id)){
+    throw  obj = {
+      statusCode: 422,
+      message: `ID É uma texto, mas deveria ser apenas número. ID: ${id}`,
+      status: "Unprocessable Entity",
+      details: ""
+    };
+  }
   let planilhaId = spreadSheetID;
   let planilha = SpreadsheetApp.openById(planilhaId);
   let guia = planilha.getSheetByName(sheetName); 
@@ -128,8 +153,13 @@ function buscarPorId(id) {
   let objeto;
   
   let lastRow = sheet.getLastRow();
-  if (lastRow < id) {
-    throw new Error("Id não encontrado.");
+  if (lastRow < id || id < 2) {
+    throw  obj = {
+      statusCode: 404,
+      message: `ID de pedido não encontrado. ID: ${id}`,
+      status: "Object Not Found",
+      details: ""
+    };
   }
 
   let valorColunaA = guia.getRange("A" + linha).getValue();
@@ -172,18 +202,22 @@ function encontrarTextoNaFolha(str) {
     let valorColunaA = guia.getRange("A" + linha).getValue();
     let valorColunaB = guia.getRange("B" + linha).getValue();
     let valorColunaC = guia.getRange("C" + linha).getValue();
+    let valorColunaE = guia.getRange("E" + linha).getValue();
     let valorColunaF = guia.getRange("F" + linha).getValue();
     let objeto = {
-      A: valorColunaA,
-      B: valorColunaB,
-      C: valorColunaC,
-      F: valorColunaF
+      id: valorColunaA,
+      data: valorColunaB,
+      unidade: valorColunaC,
+      itens: valorColunaE,
+      tipoPedido: valorColunaF
     };
     Logger.log('Resultado Pesquisa: '+ valorColunaA+' : '+valorColunaC);
     if (!listaDeObjetos.some(item => JSON.stringify(item) === JSON.stringify(objeto))) {
       listaDeObjetos.push(objeto);
     }
   }
+  listaDeObjetos  .sort((a, b) => b.id - a.id);
+
   return listaDeObjetos;
   
 }
@@ -199,7 +233,12 @@ function encontrarTextoNaColuna(str) {
   let resultados = textFinder.findAll();
 
   if (resultados.length == 0){
-    throw new Error("Nenhum objeto encontrado.");
+    throw  error = {
+      statusCode: 404,
+      message: `Nenhum resultado encontrado. Pesquisa: ${str}`,
+      status: "Object Not Found",
+      details: ""
+    };
   }
 
   let listaDeObjetos = [];
@@ -231,13 +270,18 @@ function retornarItensPaginados(paginaAtual, elementosPorPagina) {
   let totalPaginas = Math.ceil(lastRow / elementosPorPagina);
 
   if (paginaAtual < 1 || paginaAtual > totalPaginas || paginaAtual < 0) {
-    throw new Error("Página fora do limite.");
+    throw  error = {
+      statusCode: 404,
+      message: `Página fora do limite. Página: ${paginaAtual}`,
+      status: "Not Found",
+      details: ""
+    };
   }
 
   let maxInicio = (paginaAtual - 1) * elementosPorPagina + 1;
   let maxFim = maxInicio + elementosPorPagina - 1;
 
-  // Certifica-se de que os índices não ultrapassem o último elemento
+  // Certificar de que os índices não ultrapassem o último elemento
   maxFim = Math.min(maxFim, lastRow);
 
   let range = sheet.getRange(maxInicio, 1, maxFim - maxInicio + 1, 3);
@@ -265,8 +309,13 @@ function retornarItensPaginadosOrdemInversa(paginaAtual, elementosPorPagina) {
 
   let totalPaginas = Math.ceil(lastRow / elementosPorPagina);
 
-  if (paginaAtual < 1 || paginaAtual > totalPaginas || paginaAtual < 0) {
-    throw new Error("Página fora do limite.");
+  if (paginaAtual < 1 || paginaAtual > totalPaginas || paginaAtual < 0) {    
+     throw  error = {
+      statusCode: 404,
+      message: `Página fora do limite. Página: ${paginaAtual}`,
+      status: "Not Found",
+      details: ""
+    };
   }
 
   let maxFim = lastRow - (paginaAtual - 1) * elementosPorPagina;
@@ -303,9 +352,13 @@ function retornarItensIntervalo(inicio, fim) {
   let maxInicio = Math.min(inicio, lastRow);
   let maxFim = Math.min(fim, lastRow);
   if (maxInicio > maxFim) {
-    throw new Error(
-      `O parâmetro inicio não pode ser maior que o parâmetro fim, inicio: ${maxInicio}, fim: ${maxFim}`
-      );
+     throw  error = {
+      statusCode: 422,
+      message: 
+      `O parâmetro inicio (startId) não pode ser maior que o parâmetro fim (endId), inicio: ${maxInicio}, fim: ${maxFim}`,
+      status: "Unprocessable Entity",
+      details: ""
+    };
   }
   if (maxInicio == 1) {
     maxInicio = lastRow - 9;
@@ -347,3 +400,5 @@ function retornarTodosColunasABC() {
 function ultimaLinha() {
   return sheet.getLastRow();
 }
+
+// https://developers.google.com/apps-script/reference/spreadsheet/sheet?hl=pt-br
