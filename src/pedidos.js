443574e543623866081217180d33aeb97a34ee60
leandromelolm <leandrosiqueira.pe@gmail.com>
2023-11-11 21:29:05 -0300

    refactor(pedidos): mostrarPedido

--- src/pedidos.js
+++ src/pedidos.js
@@ -1,76 +1,65 @@
 let res_AppScript;
 let res_UrlSpreadSheet;
 const apiUrlLastRow = 'https://script.google.com/macros/s/AKfycbyBBzepvuBTF414jIiZ6xbq89RFcYvW36yRR4yxarOsXoqFaLGvDRIf5FkSWCBNK3m5NQ/exec';
+let ultimaAtualizacaoDaPagina = document.getElementById('ultimaAtualizacaoDaPagina');
+let msgNovoPedido = document.getElementById('msgNovoPedido');
+let abrirPedidonoFormulario = document.getElementById('abrirPedidonoFormulario');
 let lastRowOrder;
-window.onload = () => {    
+let id = "";
+let search = "";
+let page = 1;
+let perPage = 20;
+let startId = 1; // startId e endId apenas são validos na pesquisa quando search for atribuido o valor 'pesquisarIntervalo'
+let endId = 1;
+let ultimoPedidoFeito;
 
-    // GET
-    let id = "";
-    let search = "";
-    let page = 1;
-    let perPage = 20;
-    let startId = 1; // startId e endId apenas são validos na pesquisa quando search for atribuido o valor 'pesquisarIntervalo'
-    let endId = 1;
-    fetch(`/.netlify/functions/fetch-spreadsheet?id=${id}&search=${search}&page=${page}&perPage=${perPage}&startId=${startId}&endId=${endId}`)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(data) {
-            preencherTabelaListaDePedidos(data.res.data);
-        })
+window.onload = () => {
+    showLoading();
+    ultimoPedidoFeito = localStorage.getItem('ultimoPedido');
+    
+    fetch(apiUrlLastRow).then(response =>{
+        return response.json();
+    })
+    .then(data => {
+        
+        if ( data.lastRow != ultimoPedidoFeito || ultimoPedidoFeito == null){
+            localStorage.setItem('ultimoPedido', data.lastRow);
+            fetch(`/.netlify/functions/api-spreadsheet?id=${id}&search=${search}&page=${page}&perPage=${perPage}&startId=${startId}&endId=${endId}`)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data) {
+                localStorage.setItem('listaDePedidos',JSON.stringify(data.res.data));
+                preencherTabelaListaDePedidos(data.res.data);
+                ultimaAtualizacaoDaPagina.innerText = `última atualização: ${dateFormat(new Date())}`;                
+            })
+            .catch(function (error) {
+                alert(error);
+                console.log(error)
+            });
+        }else{
+            console.log("Preenchido com dados do localStorage");
+            let listaDePedidos = localStorage.getItem('listaDePedidos');
+            preencherTabelaListaDePedidos(JSON.parse(listaDePedidos));
+            ultimaAtualizacaoDaPagina.innerText = `última atualização: ${dateFormat(new Date())}`;
+            msgNovoPedido.innerText = 'nenhum novo pedido';
+        }
+    })
     
     // POST
-    submitEmail("TEST@TEST.EMAIL");
+    submitPostTest("OI test");
 
-
-    // fetch(apiUrlLastRow)
-    //     .then(response =>{
-    //     return response.json();
-    //     })
-    //     .then(data =>{
-    //         dados = recuperarListaDePedidosLocalStorage();
-    //         if (dados) {
-    //             preencherTabelaListaDePedidos(dados);
-    //             console.log(data.lastRow);
-    //         }
-    //         lastRowOrder = data.lastRow;
-    //         // verificarDados(data.lastRow);
-    //         console.log(data.lastRow);
-    //         // fetchPostGetSheetData('ultimopedido', '');
-    //         fetchPostGetSheetData('obter', `A${data.lastRow - 20}:C${data.lastRow}`);
-    // })
-
-
-    // fetch(`/.netlify/functions/fetch-spreadsheet`)
-    //     .then(function (response) {
-    //         return response.json();
-    //     })
-    //     .then(function (res) {                      
-    //        resAppScript = res.appscript;
-    //        restUrlSpreadSheet = res.urlspreadsheet;
-    //        console.log(res.testenv);
-    //        dados = recuperarListaDePedidosLocalStorage();
-    //        if (dados) {
-    //             preencherTabelaListaDePedidos(dados);
-    //        }
-    //        fetchPostGetSheetData('ultimopedido', '');
-    //     })
-    //     .catch(function (error) {
-    //         alert(error);
-    //         console.log(error)
-    //     });
 };
 
-function submitEmail(email) {
-    fetch('/.netlify/functions/fetch-spreadsheet', {
+function submitPostTest(str) {
+    fetch('/.netlify/functions/api-spreadsheet', {
         method: 'post',
         body: JSON.stringify({
-          email: email
+            str: str
         })
       }).then(function(response) {
         return response.json()
       }).then(function(data) {
-        console.log('data from function (POST)', data)
         res_AppScript = data.appscript;
         res_UrlSpreadSheet = data.urlspreadsheet;
       })
@@ -82,21 +71,13 @@ formularioPequisa.addEventListener('submit', e => {
     pesquisar();   
 })
 
-let itemInicial;
-let ultimoPedido;
 function pesquisar(){
     let txtLinhaPesquisada = document.getElementById("textoPesquisado").value;
-    let txtlinhaFormatada = `A${txtLinhaPesquisada}:F${txtLinhaPesquisada}`;
     if(!isNumberGreaterThanOne(txtLinhaPesquisada)){
-        document.getElementById("textoPesquisado").value = "";
-        return alert("Apenas é possível pesquisar pelo número do pedido");
-    } else
-    if(!txtLinhaPesquisada == ''){
-        fetchPostGetSheetData('obter', txtlinhaFormatada);
+        fetchGetSheetData("", txtLinhaPesquisada,"","","","");
+    } else {
+        fetchGetSheetData(txtLinhaPesquisada,"","","","","");
     }
-    else{
-        fetchPostGetSheetData('ultimopedido', '');      
-    }   
     desabilitarBotaoPesquisa(); // desabilita por 3 segundos
 };
 
@@ -113,22 +94,19 @@ function isNumberGreaterThanOne(value) {
     return true;
 }
 
-function fetchPostGetSheetData(tipoRequisicao, obterCelulas){
+function fetchGetSheetData(id, search, page, perPage, startId, endId){
     showLoading();
     let linkScript = res_AppScript;
     const linkPlanilha = res_UrlSpreadSheet;
-    fetch(linkScript, {
-        method: 'POST',
-        body: JSON.stringify({
-          link: linkPlanilha,
-          pagina: 'sheet1',
-          celulas: obterCelulas,
-          requisicao: tipoRequisicao,
-          sheetName: 'Sheet1'
-        }),    
+    fetch(
+        `/.netlify/functions/api-spreadsheet?id=${id}&search=${search}&page=${page}&perPage=${perPage}&startId=${startId}&endId=${endId}`
+    )
+    .then( data => {
+        return data.json();
     })
-    .then((response) => response.json()) 
-    .then((data) => verificarDados(data))
+    .then( response =>{
+       verificarDados(response);
+    })
     .catch(function (error) {
         alert(`Erro na Requisição: ${error}`);
         document.getElementById('divListaPedido').innerHTML = "Pedido não encontrado. Insira um número de pedido válido.";
@@ -138,55 +116,16 @@ function fetchPostGetSheetData(tipoRequisicao, obterCelulas){
 function isNumber(n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
 }
+function verificarDados(data){
+    if (Array.isArray(data.res.results)) {
+        const itens = data.res.results;
+        preencherTabelaListaDePedidos(itens);
+        hideLoading();
+    } else {
+        mostrarPedido(data.res);
+        hideLoading();
+    }
 
-function verificarDados(dados){    
-    let listPedidosRecebido;
-    const ultPedido = localStorage.getItem('ultimoPedido', dados);
-    if (ultPedido == dados) {      
-        console.log("verificarDados1");  
-        dados = recuperarListaDePedidosLocalStorage();
-        ultimoPedido = ultPedido;
-        document.querySelector('#numeroUltimoPedido').innerHTML = `Último pedido: ${ultPedido}`;
-    }
-    if (typeof dados === 'number') {       
-        localStorage.setItem('ultimoPedido', dados);
-        document.querySelector('#numeroUltimoPedido').innerHTML = `
-        Parece ter um novo pedido.
-        Último pedido: ${dados}`;
-        ultimoPedido = dados;       
-        if (dados < 22) {            
-            itemInicial = ultimoPedido - dados;
-            fetchPostGetSheetData('obter', `A${itemInicial + 2}:C${ultimoPedido}`);
-        } else {           
-            fetchPostGetSheetData('obter', `A${ultimoPedido - 20}:C${ultimoPedido}`);
-        }
-    }
-    if (typeof dados !== 'number') {        
-        hideLoading(); 
-        listPedidosRecebido = dados;
-        
-        let abrirPedidonoFormulario = document.getElementById('abrirPedidonoFormulario');
-        let divPedidoBuscado = document.getElementById('divPedidoBuscado');
-        let listaOrdenadaItemPedido = document.getElementById('listaOrdenadaItemPedido');
-        divPedidoBuscado.className = 'card';
-        document.getElementById('divListaPedido').innerHTML = "";
-        if(listPedidosRecebido.length > 1){
-        // if(Array.isArray(listPedidosRecebido)){
-            preencherTabelaListaDePedidos(listPedidosRecebido);
-            abrirPedidonoFormulario.className = "d-none";
-            localStorage.setItem('listaDePedidos', JSON.stringify(listPedidosRecebido))
-        }
-        if(listPedidosRecebido[0][0] == ''){
-            document.getElementById('divListaPedido').innerHTML = "Pedido não encontrado";
-            divPedidoBuscado.className = 'd-none';
-            abrirPedidonoFormulario.className = "d-none";
-            listaOrdenadaItemPedido.innerHTML = '';
-        }
-        if (listPedidosRecebido.length == 1 && listPedidosRecebido[0][0] !== '') {
-            abrirPedidonoFormulario.className = "btn btn-primary armazenamento";            
-            preencherTabelaPedidoBuscado(listPedidosRecebido);
-        }
-    };
 }
 
 function recuperarListaDePedidosLocalStorage() {
@@ -195,23 +134,22 @@ function recuperarListaDePedidosLocalStorage() {
     return objListaDePedido;
 }
 
-function preencherTabelaPedidoBuscado(data) {    
+function mostrarPedido(pedido) {
+    abrirPedidonoFormulario.className = "btn btn-primary armazenamento";
+    document.getElementById('divListaPedido').innerHTML = "";
+    document.getElementById('divPedidoBuscado').className = 'card';
+    document.getElementById('idPedido').innerHTML = `<b>Número Pedido</b>: ${pedido.id}`;
+    document.getElementById('dataPedido').innerHTML = `<b>Data Pedido</b>: ${dateFormat(pedido.dataPedido)}`;
+    document.getElementById('requisitante').innerHTML = `<b>Unidade Requisitante</b>: ${pedido.nomeUnidade}`;
     const tabela = document.getElementById("data-table").getElementsByTagName('tbody')[0];
     while (tabela.firstChild) {
         tabela.removeChild(tabela.firstChild);
     }
-    document.getElementById('idPedido').innerHTML = `<b>Número Pedido</b>: ${data[0][0]}`;
-    document.getElementById('dataPedido').innerHTML = `<b>Data Pedido</b>: ${dateFormat(data[0][1])}`;
-    document.getElementById('requisitante').innerHTML = `<b>Unidade Requisitante</b>: ${data[0][2]}`;
-    document.getElementById('unidadeRequisitante').value = data[0][3];
-    document.getElementById('listaPedido').value = data[0][4];   
-    const listaItens = data[0][4];    
-    let dados = JSON.parse(listaItens);
-    dados.forEach((obj, index) => {
+    const listaItens =pedido.itens; 
+    listaItens.forEach((obj, index) => {
         const row = tabela.insertRow();        
         const itemCell = row.insertCell();
-        itemCell.textContent = index + 1; // Números crescentes a partir de 1
-      
+        itemCell.textContent = index + 1; // Números crescentes a partir de 1      
         for (const key in obj) {
           if (obj.hasOwnProperty(key)) {
             const cell = row.insertCell();
@@ -221,7 +159,9 @@ function preencherTabelaPedidoBuscado(data) {
     });
 }
 
-function preencherTabelaListaDePedidos(arr) {    
+function preencherTabelaListaDePedidos(arr) {
+    hideLoading();
+    abrirPedidonoFormulario.className = "d-none";
     document.getElementById('divPedidoBuscado').className = 'd-none';
     document.getElementById('divListaPedido').innerHTML = "";
     const tabelaDiv = document.getElementById('divListaPedido');
@@ -235,8 +175,6 @@ function preencherTabelaListaDePedidos(arr) {
     dataHeader.textContent = 'Data';
     const unidadeHeader = document.createElement('th');
     unidadeHeader.textContent = 'Unidade';
-    // const tipoPedidoHeader = document.createElement('th');
-    // tipoPedidoHeader.textContent = 'Tipo';
 
     theadRow.appendChild(ordemHeader);
     theadRow.appendChild(dataHeader);
@@ -247,7 +185,6 @@ function preencherTabelaListaDePedidos(arr) {
     arr.forEach((item) => {
         const row = table.insertRow();
         const cell = row.insertCell();
-        console.log(item);
         const link = document.createElement('a');
         link.textContent = item.id; 
         link.href = 'javascript:void(0)';
@@ -272,7 +209,7 @@ function preencherTabelaListaDePedidos(arr) {
         linkUnid.style.fontSize = '15px';
 
         linkUnid.addEventListener('click', function () {
-            document.getElementById("textoPesquisado").value = item[0];                    
+            document.getElementById("textoPesquisado").value = item.id;                    
             pesquisar();
             scrollToTop();
         });
@@ -333,13 +270,16 @@ function limparTodosCampos(){
     document.getElementById("textoPesquisado").value = "";
     divPedidoBuscado = document.getElementById('divPedidoBuscado');
     divPedidoBuscado.className = 'd-none table';
-    fetchPostGetSheetData('ultimopedido', '');
+    let listaDePedidos = localStorage.getItem('listaDePedidos');
+    preencherTabelaListaDePedidos(JSON.parse(listaDePedidos));
     
 };
 
-function dateFormat(data) {
+function dateFormat(data) {   
     const dataObj = new Date(data);
-    
+    if (dataObj.toString() == "Invalid Date"){
+        return `Não foi possível verificar a data`;
+    }    
     const dia = String(dataObj.getDate()).padStart(2, '0');
     const mes = String(dataObj.getMonth() + 1).padStart(2, '0');
     const ano = dataObj.getFullYear();
