9500b611962e71181cea55f6ec6c555ea3a92284
leandromelolm <leandrosiqueira.pe@gmail.com>
2023-12-08 16:28:18 -0300

    feat(inventario-lista): findByProtocolo

--- src/inventario-lista.js
+++ src/inventario-lista.js
@@ -29,7 +29,7 @@ function divListGroup(res){
     let item = [];
     res.content.data.forEach(e => {
         item.push(`
-        <a href="#" class="list-group-item list-group-item-action">
+        <a href="#divSearch" class="list-group-item list-group-item-action">
             <div class="d-flex w-100 justify-content-between">
                 <h5 class="mb-1">${e.unidade}</h5>
                 <small class="text-muted">${e.id}</small>
@@ -44,3 +44,109 @@ function divListGroup(res){
     // localStorage.setItem('inventario-lista_enviados', JSON.stringify(res.content.data));
     // localStorage.setItem('inventario-lista_enviados_tamanho', res.content.data.length);
 }
+
+function eventClickEnter(event) {
+    if (event.keyCode === 13) {
+        handleSearch();
+    }
+}
+
+function handleSearch() {
+    let protocolo = document.getElementById('search-input').value;
+    getApiByProtocolo(protocolo);
+}
+
+function getApiByProtocolo(protocolo) {
+    document.getElementById('divLoadingById').classList.remove('d-none');
+    let api = "https://script.google.com/macros/s/AKfycbyBWMDtbaUzoaWZ1tI7g70e5gNvDdsEIRhGu0fPDvMaW454TvUv8tCB626H5d9tTwm5Ag/exec";
+    apiParam = `${api}?protocolo=${protocolo}`
+    fetch(apiParam,{ 
+        method: "GET",
+    })
+    .then(response =>response.json())
+    .then(data => getByProtocoloResponse(data))
+    .catch(error => msgErro(error));
+}
+
+function msgErro(error) {
+    console.log(error);
+    alert(`
+    Erro ao buscar. Protocolo pesquisado talvez não exista.
+    `)
+}
+
+function getByProtocoloResponse(res) {
+    document.getElementById('divLoadingById').classList.add('d-none')
+    let itensInventario = JSON.parse(res.content.itensInventario);
+    criarTabelaInventario(itensInventario);
+    cabecalhoInventario(res.content);
+    criarCardInformaçõesAdicionais(res.content.informacoesAdicionais);
+}
+
+function cabecalhoInventario(e) {
+    let headerInventary = document.getElementById('headerInventary');
+    let dataFormatada = dateFormat(e.data);
+    headerInventary.innerHTML = `
+        <span href="#" class="list-group-item list-group-item-action span__header-inventary">
+            <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">
+                    <b>${e.unidade}</b>
+                </h5>
+                <small class="text-muted">${e.id}</small>
+            </div>
+            <p class="mb-1">${dataFormatada}</p>
+            <small class="text-muted">${e.responsavel}</small>
+        </span>       
+        `
+    document.getElementById('itemLoading').style.display = 'none';    
+}
+
+function criarTabelaInventario(itensInventario) {
+    const tabelaInventario = document.getElementById('tabelaInventario');
+    tabelaInventario.innerHTML = '';
+    const cabecalho = tabelaInventario.createTHead();
+    const cabecalhoLinha = cabecalho.insertRow();
+    const rotulos = ['Item', 'Descrição', 'Estoque', 'Pedido'];
+    rotulos.forEach((rotulo) => {
+        const th = document.createElement('th');
+        th.textContent = rotulo;
+        cabecalhoLinha.appendChild(th);
+    });
+    itensInventario.forEach((item) => {
+        const linha = tabelaInventario.insertRow();
+        for (const chave in item) {
+            const celula = linha.insertCell();
+            celula.textContent = item[chave];
+        }
+    });
+}
+
+function criarCardInformaçõesAdicionais(e){
+    const strQuebraLinha = e.replace(/\n/g, '<br>');
+    let informacoesAdicionais = document.getElementById('informacoesAdicionais');
+    informacoesAdicionais.innerHTML = `
+        <span class="list-group-item list-group-item-action span__info-add">
+            <div class="d-flex w-100 justify-content-between">
+                <span class="mb-1">
+                    <b>Informações adicionais:</b>
+                </span>
+            </div>
+            <p class="mb-1">${strQuebraLinha}</p>
+        </span>       
+        `
+}
+
+function dateFormat(data) {   
+    const dataObj = new Date(data);
+    if (dataObj.toString() == "Invalid Date"){
+        return `Não foi possível verificar a data`;
+    }    
+    const dia = String(dataObj.getDate()).padStart(2, '0');
+    const mes = String(dataObj.getMonth() + 1).padStart(2, '0');
+    const ano = dataObj.getFullYear();
+    const horas = String(dataObj.getHours()).padStart(2, '0');
+    const minutos = String(dataObj.getMinutes()).padStart(2, '0');
+    const segundos = String(dataObj.getSeconds()).padStart(2, '0');
+    
+    return `${dia}-${mes}-${ano} ${horas}:${minutos}:${segundos}`;
+};
