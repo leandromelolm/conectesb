acb20f9a317e81561226582f510e96ec524ac0e2
Leandro Melo <leandrosiqueira.pe@gmail.com>
2023-12-22 09:10:40 -0300

    refactor(api-spreadsheet): add lib node-fetch, post alterado no retorno, add get ..inventario/all

--- functions/api-spreadsheet/api-spreadsheet.js
+++ functions/api-spreadsheet/api-spreadsheet.js
@@ -1,14 +1,12 @@
 // import axios from "axios";
 const axios = require("axios");
 const apiLastRow = require('./api/lastRow.js');
-
+const inventario = require('./api/inventario.js');
+const fetch = require('node-fetch');
 
 exports.handler = async function (event, context, callback) {
   const script = process.env.APP_SCRIPT_GOOGLE;
-  const urlplanilha = process.env.URL_PLANILHA_GOOGLE;
   const testEnv = process.env.TEST_ENV;
-  console.log(testEnv);
-
 
   if (event.httpMethod === "POST") {
     try {
@@ -23,34 +21,63 @@ exports.handler = async function (event, context, callback) {
       });  
       if (!response.ok) {
         throw new Error('Erro na requisição: ' + response.status);
-      }  
-      const data = await response.text();
-      console.log(data);
-      let obj = JSON.parse(data);
-      const responseBody = JSON.stringify({ 
-        message: 'Requisição bem-sucedida', 
-        numeroPedido: obj.row,
-        dataPedido: param.date
-      });
-      const responseObject = {
-        statusCode: 200,
-        body: responseBody
-      };  
-      return responseObject;
+      } else {
+        const data = await response.text();
+        console.log(data);
+        let obj = JSON.parse(data);
+        const responseBody = JSON.stringify({ 
+          message: 'Requisição bem-sucedida', 
+          numeroPedido: obj.row,
+          dataPedido: param.date
+        });
+        const responseObject = {
+          statusCode: 200,
+          body: responseBody
+        };  
+        return responseObject;
+      }
     } catch (error) {
       console.error('Erro durante a requisição:', error);
       return {
         statusCode: 500,
-        body: JSON.stringify({ error: error.message })
+        body: JSON.stringify({ error: error.message, success: false })
       };
     }
   }
-  
 
+  const path = event.path.replace(/\.netlify\/functions\/[^/]+/, '')
+  const segments = path.split('/').filter(e => e)
+
+  /* GET {domain}/.netlify/functions/api-spreadsheet/inventario */ 
+  if (segments[0] === 'inventario') {
+    /* GET {domain}/.netlify/functions/api-spreadsheet/inventario/all */
+    if (segments[1] === "all") {
+      let response1 = await inventario();
+      console.log("RESPONSE1", response1.body);
+      let responseData = JSON.parse(response1.body);
+
+      // REQUISIÇÃO DIRETA
+      // let response = await axios.get(`${process.env.API_GS_INVENTARIO}?search=all`);
+      // console.log(JSON.stringify(response.data));
+      // let responseData = response.data;
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify(responseData),
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json;charset=UTF-8'
+        }
+      };
+    }
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ segments }),
+    };
+  }  
+  
   if(event.httpMethod == "GET") {
 
-    /* GET /.netlify/functions/api-spreadsheet?id=&search=&page=&perPage=&startId=&endId=&search_key= */
-    
     let id = event.queryStringParameters.id;
     let search = event.queryStringParameters.search;
     let page = event.queryStringParameters.page;
@@ -59,6 +86,7 @@ exports.handler = async function (event, context, callback) {
     let endId = event.queryStringParameters.endId;
     let lastRow = event.queryStringParameters.lastRow;
     
+    /* GET {domain}/.netlify/functions/api-spreadsheet?lastRow=true */
     if (lastRow) {
       let res = await apiLastRow(lastRow, context);    
       return {
@@ -67,14 +95,15 @@ exports.handler = async function (event, context, callback) {
       };
     }
    
+    /* GET {domain}/.netlify/functions/api-spreadsheet?id=&search=&page=&perPage=&startId=&endId=&search_key= */
     try {  
       let response = await axios.get(
         `${script}?id=${id}&search=${search}&page=${page}&perPage=${perPage}&startId=${startId}&endId=${endId}`
         );
-      let responseData = response.data;
+      let responseDataPedidos = response.data;
       return {
         statusCode: 200,
-        body: JSON.stringify({ responseData }),
+        body: JSON.stringify({ responseDataPedidos }),
       };
     } catch (error) {
       
